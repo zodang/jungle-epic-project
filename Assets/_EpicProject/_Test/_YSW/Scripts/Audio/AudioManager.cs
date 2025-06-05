4a13f628527c08b42e3787af073080b727973f3e
YS1NE <165553172+YS1NE@users.noreply.github.com>
2025-06-05 17:23:53 +0900

    [FEAT] 기본적인 오디오 시스템 추가
    실제 오디오 클립 추가 필요

---
+++ Assets/_EpicProject/_Test/_YSW/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,94 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    [Header("#BGM")]
+    public AudioClip bgmClip;
+    public float bgmVolume;
+    AudioSource bgmPlayer;
+    AudioHighPassFilter bgmEffect;
+
+    [Header("#SFX")]
+    public AudioClip[] sfxClips;
+    public float sfxVolume;
+    public int channels;
+    AudioSource[] sfxPlayers;
+    int channelIndex = 0;
+
+
+    public enum Sfx { } // 오디오소스 이름 쓰기
+
+
+    private void Awake()
+    {
+        instance = this;
+        Init();
+    }
+
+    void Init()
+    {
+        //배경음 플레이어 초기화
+        GameObject bgmObject = new GameObject("BgmPlayer");
+        bgmObject.transform.parent = transform;
+        bgmPlayer = bgmObject.AddComponent<AudioSource>();
+        bgmPlayer.playOnAwake = false;
+        bgmPlayer.loop = true;
+        bgmPlayer.volume = bgmVolume;
+        bgmPlayer.clip = bgmClip;
+        bgmEffect = Camera.main.GetComponent<AudioHighPassFilter>();
+
+        //효과음 플레이어 초기화
+        GameObject sfxObject = new GameObject("SfxPlayer");
+        sfxObject.transform.parent = transform;
+        sfxPlayers = new AudioSource[channels];
+        
+        for (int index=0; index < sfxPlayers.Length; index++)
+        {
+            sfxPlayers[index] = sfxObject.AddComponent<AudioSource>();
+            sfxPlayers[index].playOnAwake = false;
+            sfxPlayers[index].bypassListenerEffects = true; // 리스너 이펙트 무시
+            sfxPlayers[index].volume = sfxVolume;
+        }
+    }
+
+    public void PlayBgm(bool isPlay)
+    {
+        if (isPlay)
+        {
+            bgmPlayer.Play();
+        }
+        else
+        {
+            bgmPlayer.Stop();
+        }
+    }
+
+    public void EffectBgm(bool isPlay)
+    {
+        bgmEffect.enabled = isPlay;
+    }
+
+    public void playSfx(Sfx sfx)
+    {
+        for (int index = 0; index < sfxPlayers.Length; index++)
+        {
+            int loopIndex = (index + channelIndex) % sfxPlayers.Length;
+
+            if (sfxPlayers[loopIndex].isPlaying)
+            {
+                continue;
+            }
+
+            // 효과음 2개 이상있는 것들 랜덤 재생 여기에 구현
+
+
+
+            channelIndex = loopIndex;
+            sfxPlayers[loopIndex].clip = sfxClips[(int)sfx];
+            sfxPlayers[loopIndex].Play();
+            break;
+        }       
+    }
+}
