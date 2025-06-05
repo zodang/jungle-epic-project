42407dab43db0dac01e381e1487048f356d07c4e
zodang <1141ekdms@naver.com>
2025-06-05 20:25:42 +0900

    [Feat] InventorySlot 참조 자동화

--- Assets/_EpicProject/_Test/_CDE/BlockTest/Scripts/Clickable.cs
+++ Assets/_EpicProject/_Test/_CDE/BlockTest/Scripts/Clickable.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Clickable : MonoBehaviour, IClickable
 {
-    public List<BlockType> BlockTypeList = new List<BlockType>();
+    public List<BlockType> BlockTypeList { get; private set; } = new List<BlockType>();
 
     private PopInspectorUI _popInspectorUI;
 
