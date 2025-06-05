ea70f32065b6e74b7eb67860ededc89cbda00bb4
zodang <1141ekdms@naver.com>
2025-06-05 16:41:32 +0900

    [Chore] 폴더 구조 변경

--- Assets/_EpicProject/_Test/_CDE/Player/Scripts/InputManager.cs
+++ Assets/_EpicProject/_Test/_CDE/Player/Scripts/InputManager.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
+    public static InputManager Instance { get; private set; }
+
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
