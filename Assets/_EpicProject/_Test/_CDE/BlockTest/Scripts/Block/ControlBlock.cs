f93402bcbb7ca00ad5660f186c278a358dcb8e1a
SMG9 <asnmfg@naver.com>
2025-06-12 14:32:22 +0900

    [FEAT] Camera Framing 구현
    - 카메라 다중 목표 찰영
    - 거리에 따른 카메라 사이즈 조절
    - Background 배경 사이즈, 카메라 사이즈 동기화
    - 컨트롤 블록 탈부착 시, 다중 촬영 목표 지정

--- Assets/_EpicProject/_Test/_CDE/BlockTest/Scripts/Block/ControlBlock.cs
+++ Assets/_EpicProject/_Test/_CDE/BlockTest/Scripts/Block/ControlBlock.cs
@@ -1,11 +1,34 @@
 using Define;
 using System;
+using Unity.VisualScripting;
+using UnityEngine;
 
 public class ControlBlock : TriggerControlBase<IControllable>
 {
     public override BlockType Type => BlockType.PlayerControl;
     public override Type RequiredFeatureType => typeof(IControllable);
 
-    protected override void EnableFeature(IControllable feature) => feature.EnableControl();
-    protected override void DisableFeature(IControllable feature) => feature.DisableControl();
+    protected override void EnableFeature(IControllable feature)
+    {
+        feature.EnableControl();
+
+        CameraFraming cameraFraming = FindAnyObjectByType<CameraFraming>();
+        if (!cameraFraming.IsUnityNull() && feature is MonoBehaviour mono)
+        {
+            cameraFraming.AddTarget(mono.transform);
+        }
+    }
+    protected override void DisableFeature(IControllable feature)
+    {
+        feature.DisableControl();
+
+        CameraFraming cameraFraming = FindAnyObjectByType<CameraFraming>();
+        if (!cameraFraming.IsUnityNull() && feature is MonoBehaviour mono)
+        {
+            if (!mono.CompareTag("Player"))
+            {
+                cameraFraming.RemoveTarget(mono.transform);
+            }
+        }
+    }
 }
