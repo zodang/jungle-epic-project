00a55b17f0710b06900327018f0dd795ba790c5a
YS1NE <165553172+YS1NE@users.noreply.github.com>
2025-06-13 14:21:38 +0900

    [FEAT] 플레이어 밝기 상호작용 시 대사 플래그 추가
    PlayerFeature 스크립트 SetFlag()/

--- Assets/_EpicProject/_Test/_CDE/Player/Scripts/PlayerFeature.cs
+++ Assets/_EpicProject/_Test/_CDE/Player/Scripts/PlayerFeature.cs
@@ -77,11 +77,13 @@ public class PlayerFeature : MonoBehaviour, IControllable, IScalable, ILightAdju
             {
                 _TwinkleLv2.SetActive(true);
                 _playerAnimation.ChangeSkin(PlayerSkinType.BaldHead);
+                FlagManager.Instance.SetFlag("baldHead", true); // 예시로 baldHead 플래그 설정
             }
             else
             {
                 _TwinkleLv2.SetActive(false);
                 _playerAnimation.ChangeSkin(PlayerSkinType.Default);
+                FlagManager.Instance.SetFlag("baldHead", false); // 예시로 baldHead 플래그 해제
             }
         }
     }
