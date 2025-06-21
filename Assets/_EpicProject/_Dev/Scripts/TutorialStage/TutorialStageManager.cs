ebcd2615a932e6b2161be31f85305d1543f0dafa
zodang <1141ekdms@naver.com>
2025-06-21 22:48:32 +0900

    [Fix] 기본 블록 빌드 오류 수정

--- Assets/_EpicProject/_Dev/Scripts/TutorialStage/TutorialStageManager.cs
+++ Assets/_EpicProject/_Dev/Scripts/TutorialStage/TutorialStageManager.cs
@@ -1,5 +1,4 @@
 using Define;
-using UnityEngine.SceneManagement;
 
 public class TutorialStageManager : StageBaseManager
 {
@@ -9,11 +8,17 @@ public class TutorialStageManager : StageBaseManager
         base.Awake();
         _goalTrigger = FindAnyObjectByType<TriggerArea>();
         _goalTrigger.OnTrigger.AddListener(OnGoalTriggered);
+        
     }
 
     private void Start()
     {
-        // BGM 실행
+        GameManager.Instance.AudioManager.PlayBgm(true);
+        Invoke(nameof(CollectBlock), .05f);
+    }
+
+    private void CollectBlock()
+    {
         FindAnyObjectByType<Inventory>().Collect(BlockType.PlayerControl);
     }
 
