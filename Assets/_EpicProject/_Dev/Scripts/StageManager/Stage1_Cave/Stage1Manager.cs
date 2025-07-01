00318ba7edbc81408a8039fe5e8e830792d59842
SMG9 <asnmfg@naver.com>
2025-07-01 14:28:50 +0900

    [MOD] Stage01 데코 및 대화 기능 추가

--- Assets/_EpicProject/_Dev/Scripts/StageManager/Stage1_Cave/Stage1Manager.cs
+++ Assets/_EpicProject/_Dev/Scripts/StageManager/Stage1_Cave/Stage1Manager.cs
@@ -13,7 +13,7 @@ public class Stage1Manager : StageBaseManager
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        DialogueManager.LoadDialogue(stageFilePath + "/Dialogues");
     }
 
     // Update is called once per frame
