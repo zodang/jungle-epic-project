e49a12480942164b87ff3094eb1e882ebdccdfb2
kimminsuk <kminsuk1999@gmail.com>
2025-07-04 22:19:20 +0900

    [MOD] 글리치 비전 에셋 교체

--- Assets/_EpicProject/_Test/_KMS/Scripts/GlitchIndicator.cs
+++ Assets/_EpicProject/_Test/_KMS/Scripts/GlitchIndicator.cs
@@ -10,13 +10,6 @@ public class GlitchIndicator : MonoBehaviour
     [Header("Animator")]
     [SerializeField] private Animator _glitchAni;
 
-    [Header("Cooldown Image (RectTransform)")]
-    [SerializeField] private RectTransform _cooldownImageRT;
-
-    [Header("DOTween 세팅")]
-    [SerializeField] private float _tweenDuration = 0.3f;
-    private float _raiseY = 300f;  // 앵커 Y 이동량
-
     public Vector2 _originalAnchoredPos;
     private bool _glitchEnded = false;
 
@@ -25,7 +18,6 @@ public class GlitchIndicator : MonoBehaviour
 
     private void Start()
     {
-        _originalAnchoredPos = _cooldownImageRT.anchoredPosition; 
         GlitchVision.BeginGlitch += StartGlitchTimer;
         GlitchVision.EndGlitch += OnGlitchEnd;
     }
@@ -39,34 +31,13 @@ public class GlitchIndicator : MonoBehaviour
     private void StartGlitchTimer()
     {
         _glitchEnded = false;
-
-        // UI용 DOTween: 앵커 포지션 Y 값 변경
-        sequence = DOTween.Sequence();
-
-        sequence
-            .OnStart(() => { _glitchAni.Play("Glitch Count"); })
-            .Append(_cooldownImageRT.DOAnchorPosY(_originalAnchoredPos.y + _raiseY, _tweenDuration).SetEase(Ease.OutCubic))
-            .AppendInterval(3)
-            .Append(_cooldownImageRT.DOAnchorPosY(_originalAnchoredPos.y, _tweenDuration).SetEase(Ease.InCubic))
-            .OnComplete(() => { _glitchAni.Play("Empty");  _glitchEnded = true; })
-       ;
-
+        _glitchAni.Play("Glitch Count");
     }
 
     private void OnGlitchEnd()
     {
         _glitchEnded = true;
+        _glitchAni.Play("Empty");
     }
 
-    //private IEnumerator GlitchTimerCoroutine()
-    //{
-    //    _glitchAni.Play("Glitch Count");
-    //    yield return new WaitUntil(() => _glitchEnded);
-
-    //    // 다시 원위치로 내리기
-    //    _cooldownImageRT
-    //        .DOAnchorPosY(_originalAnchoredPos.y, _tweenDuration)
-    //        .SetEase(Ease.InCubic);
-
-    //}
 }
