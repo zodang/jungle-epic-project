a1400ca88bf489b877407d9957efee5f9ace4661
zodang <1141ekdms@naver.com>
2025-07-08 02:10:37 +0900

    [FEAT] 툴팁 활성화 delay 추가

--- Assets/_EpicProject/_Test/_CDE/Tooltip/TooltipHandler.cs
+++ Assets/_EpicProject/_Test/_CDE/Tooltip/TooltipHandler.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -5,28 +6,73 @@ public class TooltipHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitH
 {
     [SerializeField] private TooltipData _tooltipData;
     private bool _isShow = false;
+    private bool _isPointerOver;
+    
+    private Coroutine _showTooltipCoroutine;
+    
     
     public void OnPointerEnter(PointerEventData eventData)
     {
+        // 드래그 중일 때 return
+        if (Input.GetMouseButton(0) || Input.GetMouseButton(1)) return;
+            
         _isShow = true;
-        TooltipUI.Instance.ShowTooltip(_tooltipData, eventData.position);
+        _isPointerOver = true;
+        _showTooltipCoroutine = StartCoroutine(DelayTooltip(eventData.position));
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        if (_showTooltipCoroutine != null)
+        {
+            StopCoroutine(_showTooltipCoroutine);
+            _showTooltipCoroutine = null;
+        }
+        
         TooltipUI.Instance.HideTooltip();
         _isShow = false;
+        _isPointerOver = false;
     }
 
     private void Update()
     {
-        if (!_isShow) return;
-        TooltipUI.Instance.SetTooltipPosition();
+        if (Input.GetMouseButtonDown(0))
+        {
+            TooltipUI.Instance.HideTooltip();
+            _isPointerOver = false;
+            
+            if (_showTooltipCoroutine != null)
+            {
+                StopCoroutine(_showTooltipCoroutine);
+                _showTooltipCoroutine = null;
+            }
+        }
+
+        // 툴팁 위치 계속 추적
+        if (_isPointerOver)
+        {
+            TooltipUI.Instance.SetTooltipPosition();
+        }
     }
 
     private void OnDisable()
     {
+        if (_showTooltipCoroutine != null)
+        {
+            StopCoroutine(_showTooltipCoroutine);
+            _showTooltipCoroutine = null;
+        }
+        
         TooltipUI.Instance.HideTooltip();
         _isShow = false;
+    }
+
+    private IEnumerator DelayTooltip(Vector2 pos)
+    {
+        yield return new WaitForSeconds(0.5f);
+        if (_isPointerOver)
+        {
+            TooltipUI.Instance.ShowTooltip(_tooltipData, pos);
+        }
     }
 }
