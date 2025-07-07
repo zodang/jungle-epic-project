6d19e6d28dfd7195b7328f825b52175e599678bf
zodang <1141ekdms@naver.com>
2025-07-07 21:49:21 +0900

    [FEAT] 설정 음량 조절 기능 추가

--- Assets/_EpicProject/_Dev/Scripts/GameManager/AudioSetting.cs
+++ Assets/_EpicProject/_Dev/Scripts/GameManager/AudioSetting.cs
@@ -1,7 +1,47 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioSetting : MonoBehaviour
 {
     public float CurrentBgmVolume { get; private set; }
     public float CurrentSfxVolume { get; private set; }
+
+    [SerializeField] private Slider BgmSlider;
+    [SerializeField] private Slider SfxSlider;
+
+    private void Start()
+    {
+        BgmSlider.minValue = 0f;
+        BgmSlider.maxValue = GetOptimalBgmVolume() * 2f;
+        BgmSlider.value = GetOptimalBgmVolume();
+        BgmSlider.onValueChanged.AddListener(OnBgmValueChanged);
+        
+        SfxSlider.minValue = 0f;
+        SfxSlider.maxValue = GetOptimalSfxVolume() * 2f;
+        SfxSlider.value = GetOptimalSfxVolume();
+        SfxSlider.onValueChanged.AddListener(OnSfxValueChanged);
+
+    }
+
+    private void OnBgmValueChanged(float volume)
+    {
+        CurrentBgmVolume = volume;
+        GameManager.Instance.AudioManager.SetBgmVolume(volume);
+    }
+    
+    private void OnSfxValueChanged(float volume)
+    {
+        CurrentSfxVolume = volume;
+        GameManager.Instance.AudioManager.SetSfxVolume(volume);
+    }
+
+    public float GetOptimalBgmVolume()
+    {
+        return GameManager.Instance.AudioManager.DefaultBgmVolume;
+    }
+    
+    public float GetOptimalSfxVolume()
+    {
+        return GameManager.Instance.AudioManager.DefaultSfxVolume;
+    }
 }
