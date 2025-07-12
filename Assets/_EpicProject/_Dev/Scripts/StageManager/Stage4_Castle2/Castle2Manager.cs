25a2a58dc75b65dcbd5e5bc7d8e236b16913c8a4
SMG9 <asnmfg@naver.com>
2025-07-12 17:11:23 +0900

    [FEAT] 스테이지4-2 대사 추가

--- Assets/_EpicProject/_Dev/Scripts/StageManager/Stage4_Castle2/Castle2Manager.cs
+++ Assets/_EpicProject/_Dev/Scripts/StageManager/Stage4_Castle2/Castle2Manager.cs
@@ -25,6 +25,6 @@ public class Castle2Manager : StageBaseManager
 
     private void OnGoalTriggered()
     {
-        // GameManager.Instance.FadeManager.LoadNextScene();
+        GameManager.Instance.FadeManager.LoadNextScene();
     }
 }
