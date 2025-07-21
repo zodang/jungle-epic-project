e8273db57c9354f2c3af0b3e26db086c228ea0d0
SMG9 <asnmfg@naver.com>
2025-07-21 23:02:08 +0900

    [FIX] 이슈52: 스테이지4 살짝만 걸쳐도 내부가 보이던 문제 수정
    이슈52: 볼링공으로 내부 맵 확인이 가능한 현상
    => 맵 수정

--- Assets/_EpicProject/_Test/_SMG/Scripts/AutoYSortOrderBiasSetting.cs
+++ Assets/_EpicProject/_Test/_SMG/Scripts/AutoYSortOrderBiasSetting.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor;
@@ -5,9 +6,6 @@ using UnityEngine;
 
 public class AutoYSortOrderBiasSetting : MonoBehaviour
 {
-    float minRange;
-    float maxRange;
-
     [ContextMenu("Excute")]
     public void Excute()
     {
@@ -55,51 +53,6 @@ public class AutoYSortOrderBiasSetting : MonoBehaviour
             }
         }
     }
-
-    [ContextMenu("FindSame")]
-    public void FindSame()
-    {
-        Dictionary<float, List<YSortOrder>> dictionary = new Dictionary<float, List<YSortOrder>>();
-
-        int count = transform.childCount;
-        for (int i = 0; i < count; i++)
-        {
-            YSortOrder ySortOrder = transform.GetChild(i).GetComponent<YSortOrder>();
-            if (ySortOrder.IsUnityNull()) continue;
-
-            Transform tr = transform.GetChild(i);
-            if (!dictionary.ContainsKey(tr.position.y))
-            {
-                List<YSortOrder> ySortOrders = new List<YSortOrder>();
-                ySortOrders.Add(ySortOrder);
-                dictionary.Add(tr.position.y, ySortOrders);
-            }
-            else
-            {
-                if (dictionary.TryGetValue(tr.position.y, out var ySortOrders))
-                {
-                    ySortOrders.Add(ySortOrder);
-                }
-            }
-        }
-
-        foreach (var kvp in dictionary)
-        {
-            float yKey = kvp.Key;
-            List<YSortOrder> ySortOrders = kvp.Value;
-
-            int bias = 0;
-
-            for(int i = 0; i < ySortOrders.Count; i++)
-            {
-                for(int j = i + 1; j < ySortOrders.Count; j++)
-                {
-                    if (ySortOrders[i].sortingOrderBias == ySortOrders[j].sortingOrderBias)
-                    {
-                        Debug.Log($"ySortOrders[i].name: {ySortOrders[i].name} == ySortOrders[j].name: {ySortOrders[j].name}");
-                    }    
-                }
-            }
-        }
-    }
 }
+
+#endif
