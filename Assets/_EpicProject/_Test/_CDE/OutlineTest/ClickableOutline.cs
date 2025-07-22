1abdd693cba86cf58123606be6cf74e0b34e3882
SMG9 <asnmfg@naver.com>
2025-07-22 10:48:25 +0900

    [Build] Build 1.0.3.1_q ver

--- Assets/_EpicProject/_Test/_CDE/OutlineTest/ClickableOutline.cs
+++ Assets/_EpicProject/_Test/_CDE/OutlineTest/ClickableOutline.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
-using UnityEditor.Localization.Plugins.XLIFF.V20;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
