5fb27ebbbf51f1c77a85791608b264a23110e126
zodang <1141ekdms@naver.com>
2025-07-30 16:33:44 +0900

    [FEAT] Cave2 씬 연결
    - 로그 추가
    - 프로필 이미지 추가
    - 문 효과음 추가

--- Assets/_EpicProject/_Dev/Scripts/StageManager/Stage1_Cave2/Cave2Manager.cs
+++ Assets/_EpicProject/_Dev/Scripts/StageManager/Stage1_Cave2/Cave2Manager.cs
@@ -10,7 +10,7 @@ public class Cave2Manager : StageBaseManager
     {
         // 스테이지 정보 불러오기
         stageFilePath = "StageInfos/Cave2Stage";
-        //ChangeStageId("20_cave_1");
+        ChangeStageId("21_cave_2");
 
         base.Awake();
     }
@@ -24,36 +24,36 @@ public class Cave2Manager : StageBaseManager
 
         _goalTrigger = GameObject.FindWithTag(Tags.Goal)?.GetComponent<TriggerArea>();
         _goalTrigger.OnTrigger.AddListener(OnGoalTriggered);
-
-        //_logSectionTrigger = FindAnyObjectByType<LogSectionTrigger>();
-        //_logSectionTrigger.OnSectionTriggered += CheckPassCrack;
-
+        
         GameManager.Instance.AudioManager.PlayBgm(BgmType.Stage1);
 
         // 로그 시스템
-        //GameManager.Instance.LogManager.LogStageEnter(StageId);
-        //ChangeStageSection("20_0_enter_cave");
+        _logSectionTrigger = FindAnyObjectByType<LogSectionTrigger>();
+        _logSectionTrigger.OnSectionTriggered += CheckEndDialogue;
+        
+        GameManager.Instance.LogManager.LogStageEnter(StageId);
+        ChangeStageSection("21_0_enter_cave2");
     }
 
     private void OnGoalTriggered()
     {
         // 로그 시스템
-        //ChangeStageSection("20_2_exit_cave");
-        //GameManager.Instance.LogManager.LogStageExit(StageId, SectionId, "clear", Time.realtimeSinceStartup - StageStartTime);
+        ChangeStageSection("21_2_exit_cave2");
+        GameManager.Instance.LogManager.LogStageExit(StageId, SectionId, "clear", Time.realtimeSinceStartup - StageStartTime);
 
         GameManager.Instance.AudioManager.FadeOutAudio(1.0f);
         GameManager.Instance.FadeManager.LoadNextScene();
     }
 
-    private void CheckPassCrack()
+    private void CheckEndDialogue()
     {
         // 로그 시스템
-        //ChangeStageSection("20_1_pass_crack");
+        ChangeStageSection("21_1_end_dialogue");
     }
 
     protected override void OnDestroy()
     {
-        //_logSectionTrigger.OnSectionTriggered -= CheckPassCrack;
+        _logSectionTrigger.OnSectionTriggered -= CheckEndDialogue;
         base.OnDestroy();
     }
 }
