e46404d902e5e4343c618ea8c27850891f068270
zodang <1141ekdms@naver.com>
2026-06-17 21:04:54 +0900

    [REFACTOR] 빈 monobehaviour 함수 삭제

--- Assets/_EpicProject/_Dev/Scripts/StageManager/Stage4_Castle2/Castle2UIManager.cs
+++ Assets/_EpicProject/_Dev/Scripts/StageManager/Stage4_Castle2/Castle2UIManager.cs
@@ -2,15 +2,4 @@ using UnityEngine;
 
 public class Castle2UIManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
